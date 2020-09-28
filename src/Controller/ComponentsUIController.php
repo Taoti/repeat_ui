6609fe24bcf84c6e3b3b506286f73ac12acc5f9d
Eric Aguayo <ericmaster@nimblersoft.com>
2020-09-28 08:05:25 -0500

    UI updated

--- src/Controller/ComponentsUIController.php
+++ src/Controller/ComponentsUIController.php
@@ -6,6 +6,7 @@ use Drupal\Core\Controller\ControllerBase;
 use RepeatCli\Components;
 use Drupal\Core\Render\Markup;
 use Symfony\Component\Yaml\Yaml;
+use ScssPhp\ScssPhp\Compiler;
 
 define('LIPSUM', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
 do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
@@ -44,16 +45,30 @@ class ComponentsUIController extends ControllerBase {
     // Load the Twig theme service.
     /** @var \Twig_Environment $twig_service */
     $twig_service = \Drupal::service('twig');
+    $scss = new Compiler();
+
+    $scss_var_defaults = '
+      $blue: #339;
+      $gray: #999;
+      $white: #fff;
+      $box-shadow: 0 0 #ccc;
+      $width--m: 100%;
+      $width--l: 100%;';
 
     // Parse repeate components directory.
     $components = array_diff(scandir($this->sourceDirectory), ['..', '.']);
     foreach ($components as $component_name) {
-      $component_dir = $this->sourceDirectory . '/' . $component_name .
+      $template_dir = $this->sourceDirectory . '/' . $component_name .
         '/templates/canonical';
-      $template_filename = reset(array_diff(scandir($component_dir),
+      $scss_dir = $this->sourceDirectory . '/' . $component_name .
+        '/sass/3.components';
+
+      // Get the twig dir.
+      $template_filename = reset(array_diff(scandir($template_dir),
         ['..', '.']));
-      $template_content = file_get_contents($component_dir . '/' .
+      $template_content = file_get_contents($template_dir . '/' .
         $template_filename);
+
       // @TODO: load context variables... somehow...
       $context = [
         'name' => $component_name,
@@ -62,12 +77,30 @@ class ComponentsUIController extends ControllerBase {
       if (strpos($template_content, 'field_target_entity') === FALSE &&
           strpos($template_content, 'field_value') === FALSE) {
         if ($component_name == 'accordion'){
+          // Get the styles.
+          $scss_filename = reset(array_diff(scandir($scss_dir),
+            ['..', '.']));
+          if (!empty($scss_filename)) {
+
+            $scss_content = file_get_contents($scss_dir . '/' .
+              $scss_filename);
+            $css = $scss->compile($scss_var_defaults . $scss_content);
+          }
+          $context['classes'] = [
+            'accordion',
+            'accordion--collapsed',
+            'paragraph',
+            'paragraph--type--accordion-item',
+            'paragraph--view-mode--default',
+          ];
           $context['section_title'] = 'Section Title';
           $context['title'] = 'Title';
           $context['accordion_content'] = LIPSUM;
           $build[$component_name] = [
             '#type' => 'inline_template',
-            '#template' => '<h2>{{name}}</h2>' . $template_content,
+            '#template' => '<h2>{{name}}</h2>' .
+              '<style>' . $css . '</style>' .
+              $template_content,
             '#context' => $context,
           ];
         }
